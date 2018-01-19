61f0b01c894e846666b32943d46fce29470d03cf
Ximura <orukin@gmail.com>
2018-01-20 00:15:34 +0200

    add tests

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ctci
+
+[![Build Status](https://travis-ci.org/ximura/ctci.svg?branch=wip_py)](https://travis-ci.org/ximura/ctci)
+[![codecov](https://codecov.io/gh/ximura/ctci/branch/wip_py/graph/badge.svg)](https://codecov.io/gh/ximura/ctci)
+
 ==== Solutions for "Cracking the Coding Interview v5"
--- python/test/chapter_1/test_problem_1.py
+++ python/test/chapter_1/test_problem_1.py
@@ -7,6 +7,6 @@ def test_character_uniqueness(text, result):
     assert unique_characters(text) == result
 
 
-@pytest.mark.parametrize("text, result",  [("aca", False)])
-def test_character_uniqueness(text, result):
+@pytest.mark.parametrize("text, result",  [("abc", True), ("aac", False), ("aBbc", True)])
+def test_character_uniqueness_no_cache(text, result):
     assert unique_characters_no_cache(text) == result
